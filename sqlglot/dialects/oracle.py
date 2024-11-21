97efde3bebbfc324f0e4e168d70fae4a99bab523
dpp0900 <_dpp@kakao.com>
2024-11-21 10:16:40 +0000

    fix oracle table issue

--- sqlglot/dialects/oracle.py
+++ sqlglot/dialects/oracle.py
@@ -77,6 +77,7 @@ def Fts5ToContext(self: MySQL.Generator, expression: exp.Table) -> str:
         print("create_table", create_table)
         create_index = [f"CREATE INDEX idx_fts_{table}_{col} ON {table}({col}) INDEXTYPE IS CTXSYS.CONTEXT" for col in strip_col]
         return f"{create_table}; {'; '.join(create_index)}"
+    return self.table_sql(expression)
 
 class Oracle(Dialect):
     ALIAS_POST_TABLESAMPLE = True
